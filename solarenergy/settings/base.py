86bc58d50386326ac61c42b9105a5828528cbe40
Kumar Saket <saketbairoliya2@gmail.com>
2017-03-09 09:36:02 +0530

    Added mail client

--- solarenergy/settings/base.py
+++ solarenergy/settings/base.py
@@ -87,10 +87,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'saketbairoliya2@gmail.com'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
