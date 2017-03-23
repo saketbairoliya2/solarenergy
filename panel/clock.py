fcabae3f1a6438c0420d23abec694d945f167e21
Kumar Saket <saketbairoliya2@gmail.com>
2017-03-23 19:14:25 +0530

    Testing clock

--- panel/clock.py
+++ panel/clock.py
@@ -1,4 +1,5 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from django.core.mail import send_mail
 import logging
 
 # Get an instance of a logger
@@ -6,8 +7,18 @@ logger = logging.getLogger(__name__)
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+MAIL_FROM = "solarenergysaket@gmail.com"
+MAIL_TO = ['saketbairoliya2@gmail.com']
+
+@sched.scheduled_job('interval', minutes=2)
 def timed_job():
+	url = "https://solarenergy.herokuapp.com/panel/1/send_mails/?date=20-03-2017"
+	request = urllib.request.Request(url)
+	response = urllib.request.urlopen(request)
+	response = response.read().decode('utf-8')
+	subject = "Demo Mail"
+	send_mail(subject, str(response), MAIL_FROM, MAIL_TO, fail_silently=False)
+	logger.info('Mail Sent to user')
     logger.info('This job is run every three minutes.')
 
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=20)
