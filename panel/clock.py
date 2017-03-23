15577832d326c729bd1074f7b86a4d3110c8077b
Kumar Saket <saketbairoliya2@gmail.com>
2017-03-23 20:19:40 +0530

    Added task cron

--- panel/clock.py
+++ panel/clock.py
@@ -1,29 +1,33 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.mail import send_mail
+from .models import Units, Performance
+from datetime import date
+import datetime
+from django.http import HttpResponse
 import urllib.request
 import logging
+import json
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 sched = BlockingScheduler()
 
-MAIL_FROM = "solarenergysaket@gmail.com"
-MAIL_TO = ['saketbairoliya2@gmail.com']
+BASE_URL = "https://solarenergy.herokuapp.com/panel/"
 
-@sched.scheduled_job('interval', minutes=2)
-def timed_job():
-	url = "https://solarenergy.herokuapp.com/panel/1/send_mails/?date=20-03-2017"
-	request = urllib.request.Request(url)
-	response = urllib.request.urlopen(request)
-	response = response.read().decode('utf-8')
-	subject = "Demo Mail"
-	send_mail(subject, str(response), MAIL_FROM, MAIL_TO, fail_silently=False)
-	logger.info('Mail Sent to user')
-	logger.info('This job is run every two minutes.')
+# @sched.scheduled_job('interval', minutes=2)
+# def timed_job():
+# 	logger.info('This job is run every two minutes.')
 
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=20)
-# def scheduled_job():
-#     logger.info('This job is run every weekday at 8pm.')
-
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=14, minute=54)
+def scheduled_job():
+	logger.info('This job is run every day at 8:30pm.')
+	today_date = datetime.datetime.now().date()
+	date = today_date.strftime('%d-%m-%Y')
+	units = Units.objects.all()
+	for unit in units:
+		url = BASE_URL + str(unit.id) + "/send_mails?date=" + str(date)
+		request = urllib.request.Request(url)
+	return HttpResponse(json.dumps({'success': 'true'}), content_type="application/json")
+	logger.info('Daily reports sent for all panels')
 sched.start()
