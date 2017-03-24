2977e5bcbd7cd45f1da36ea69a11d06a420e65ae
Kumar Saket <saketbairoliya2@gmail.com>
2017-03-25 02:15:39 +0530

    Added scehduler for the given time

--- panel/clock.py
+++ panel/clock.py
@@ -11,16 +11,27 @@ sched = BlockingScheduler()
 
 BASE_URL = "https://solarenergy.herokuapp.com/panel/call"
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-	logger.info('This job is run every two minutes.')
+# @sched.scheduled_job('interval', minutes=3)
+# def timed_job():
+# 	logger.info('This job is run every two minutes.')
+# 	url = BASE_URL
+# 	request = urllib.request.Request(url)
+# 	response = urllib.request.urlopen(request)
+# 	response = response.read().decode('utf-8')
+# 	logger.info('Daily reports sent for all panels')
+
+# To check for IST time acceptance, basically program call
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=7, minute=30)
+def scheduled_job():
+	logger.info('This job is run every day at 8:00pm.')
 	url = BASE_URL
 	request = urllib.request.Request(url)
 	response = urllib.request.urlopen(request)
 	response = response.read().decode('utf-8')
 	logger.info('Daily reports sent for all panels')
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=14, minute=30)
+# To test for UST time acceptance
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=00)
 def scheduled_job():
 	logger.info('This job is run every day at 8:00pm.')
 	url = BASE_URL
