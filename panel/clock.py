c484b8684ec0e771f413a211eb9bd399f182c2d9
Kumar Saket <saketbairoliya2@gmail.com>
2017-03-24 07:58:15 +0530

    Added demo task

--- panel/clock.py
+++ panel/clock.py
@@ -15,18 +15,19 @@ BASE_URL = "https://solarenergy.herokuapp.com/panel/call"
 # def timed_job():
 # 	logger.info('This job is run every two minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=6, minute=00)
+#@sched.scheduled_job('cron', day_of_week='mon-sun', hour=6, minute=00)
+@sched.scheduled_job('interval', minutes=3)
 def scheduled_job():
 	logger.info('This job is run every day at 8:30pm.')
 	url = BASE_URL
 	request = urllib.request.Request(url)
 	logger.info('Daily reports sent for all panels')
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=30)
-def scheduled_job():
-	logger.info('This job is run every day at 8:30pm.')
-	url = BASE_URL
-	request = urllib.request.Request(url)
-	logger.info('Daily reports sent for all panels')
+#@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=30)
+# def scheduled_job():
+# 	logger.info('This job is run every day at 8:30pm.')
+# 	url = BASE_URL
+# 	request = urllib.request.Request(url)
+# 	logger.info('Daily reports sent for all panels')
 
 sched.start()
