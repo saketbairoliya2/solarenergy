0b3ddb21fa3ccd79cec9a567b8dc88946c67a671
Kumar Saket <saketbairoliya2@gmail.com>
2017-03-25 10:48:57 +0530

    Modified simulater function

--- panel/jobs.py
+++ panel/jobs.py
@@ -57,7 +57,7 @@ def update_power_expected_table(response, each_unit):
 		entry.unit = each_unit
 		entry.save()
 	except KeyError:
-		print("No output found in response!!")
+		logger.info("No output found in response!!")
 	return 1
 
 def simulate_energy_days_generation():
@@ -88,7 +88,7 @@ def save_power_actual(each_unit, date_now, each_power):
 
 def system_date_mapper(today_date, each_unit):
 	'''
-		This will receive date, time and System ID and will return simulated value of energy in watt
+		This will receive date and System ID and will return simulated value of energy in watt for full day, passed date can be used for getting more refined output
 	'''
 	actual_dc = get_normal_distribution(each_unit.capacity)
 	return actual_dc
@@ -98,8 +98,8 @@ def get_normal_distribution(capacity):
 		Returns simulated value of heat based on normal distribution curve in List
 	'''
 	capacity_in_watt = capacity * 1000
-	mu = (capacity_in_watt * decimal.Decimal(.70)) #Making an approximation for mean value to be 90% of system capacity
-	sigma = mu/13 # As there are 13 numbers having data like bell curve, so making equal SD.
+	mu = (capacity_in_watt * decimal.Decimal(.45)) #Making an approximation for mean value to be 90% of system capacity
+	sigma = mu/5 # As there are 13 numbers having data like bell curve, so making equal SD.
 	series = np.random.normal(mu, sigma, 13)
 	series = sorted(series)
 	series_inc = series[::2]
@@ -108,6 +108,7 @@ def get_normal_distribution(capacity):
 	energy_0_to_6 = [0, 0, 0, 0, 0, 0, 0]
 	energy_20_to_23 = [0, 0, 0, 0]
 	daily_energy = energy_0_to_6 + energy_7_to_19 + energy_20_to_23
+	print(daily_energy)
 	return daily_energy
 
 def update_performance_table():
@@ -128,19 +129,20 @@ def update_performance_table():
 		date_now = datetime.datetime.strptime(date_now, "%Y-%m-%d")
 
 		result = [performance_task(each_unit, date_now) for each_unit in units_installed if units_installed]
-		print(result)
+	logger.info('Performance table updated')
 
 def performance_task(each_unit, date_now):
 	exp_power = expected_power(each_unit, date_now)
 	act_power = list(actual_power(each_unit, date_now))
 	time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 	for each in time:
-		print(type(act_power[each]))
-		print(type(exp_power[each]))
-		if (act_power[each] < decimal.Decimal(.80)*decimal.Decimal(exp_power[each])):
-			# Insert row in Performance table
-			p = Performance(unit=each_unit, hours=each, performance_date=date_now)
-			p.save()
+		try:
+			if (act_power[each] < decimal.Decimal(.80)*decimal.Decimal(exp_power[each])):
+				# Insert row in Performance table
+				p = Performance(unit=each_unit, hours=each, performance_date=date_now)
+				p.save()
+		except IndexError:
+			logger.info('Came accross no element in performance table.')
 
 def actual_power(each_unit, date_now):
 	power_actual = PowerActual.objects.values_list('actual_dc', flat=True).filter(Q(unit=each_unit) & Q(stamp_date=date_now))
@@ -169,7 +171,7 @@ def expected_power(each_unit, date_now):
 		days_in_months += days_dict[i]
 	
 	total_days_to_ignore_for_eval = days_in_months + (day-1)
-	total_hours_to_ignore_for_eval = total_days_to_ignore_for_eval*24
+	total_hours_to_ignore_for_eval = total_days_to_ignore_for_eval*24 - 1
 	expected_power = PowerExpected.objects.filter(unit=each_unit).values('expected_dc')
 	expected_power = expected_power[0]['expected_dc']
 	expected_power = string_json_mapper(expected_power)
